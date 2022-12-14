d5cdec85060c17d04afd458c6e0a0697acc539cc
Stas Mykhailenko <stas.m@2muchcoffee.com>
2022-12-14 15:51:02 +0200

    feat (app): add task form

--- tech-test/src/app/shared/shared.module.ts
+++ tech-test/src/app/shared/shared.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   exports: [
     CommonModule,
+    ReactiveFormsModule,
+
     MatTableModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
 })
 export class SharedModule {
--- tech-test/src/app/task/task-details/task-details.component.ts
+++ tech-test/src/app/task/task-details/task-details.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskService } from '../../core/services/task.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentTask$ = this.taskService.currentTask$;
 
-  ngOnInit(): void {
+  taskForm: FormGroup;
+
+  get labelControl() {
+    return this.taskForm.get('label') as FormControl;
   }
 
+  get descriptionControl() {
+    return this.taskForm.get('description') as FormControl;
+  }
+
+  get categoryControl() {
+    return this.taskForm.get('category') as FormControl;
+  }
+
+  get doneControl() {
+    return this.taskForm.get('done') as FormControl;
+  }
+
+  constructor(
+    private taskService: TaskService,
+  ) { }
+
+  ngOnInit(): void {
+    this.taskForm = new FormGroup({
+      label: new FormControl(null),
+      description: new FormControl(null),
+      category: new FormControl(null),
+      done: new FormControl(false),
+    });
+
+    this.currentTask$.pipe(
+      filter(res => !!res),
+    ).subscribe((task) => {
+      this.taskForm.patchValue(task);
+    });
+  }
 }
