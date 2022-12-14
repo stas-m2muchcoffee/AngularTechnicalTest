73302db6cabc349feb683608f2b2738d7800cf64
Stas Mykhailenko <stas.m@2muchcoffee.com>
2022-12-14 16:25:56 +0200

    feat (app): configure testing files

--- tech-test/src/app/task/task-details/task-details.component.spec.ts
+++ tech-test/src/app/task/task-details/task-details.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NgxsModule, Store } from '@ngxs/store';
+
 import { TaskDetailsComponent } from './task-details.component';
+import { TaskState } from '../../core/ngxs/task/task.state';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TaskDetailsComponent', () => {
   let component: TaskDetailsComponent;
   let fixture: ComponentFixture<TaskDetailsComponent>;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskDetailsComponent ]
+      declarations: [ TaskDetailsComponent ],
+      imports: [
+        HttpClientTestingModule,
+        NgxsModule.forRoot([TaskState]
+      )]
     })
     .compileComponents();
   });
@@ -17,6 +26,7 @@ describe('TaskDetailsComponent', () => {
     fixture = TestBed.createComponent(TaskDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(Store);
   });
 
   it('should create', () => {
--- tech-test/src/app/task/task-list/task-list.component.spec.ts
+++ tech-test/src/app/task/task-list/task-list.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskListComponent } from './task-list.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { TaskState } from '../../core/ngxs/task/task.state';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
   let fixture: ComponentFixture<TaskListComponent>;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskListComponent ]
+      declarations: [ TaskListComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        NgxsModule.forRoot([TaskState]
+      )]
     })
     .compileComponents();
   });
@@ -17,6 +27,7 @@ describe('TaskListComponent', () => {
     fixture = TestBed.createComponent(TaskListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(Store);
   });
 
   it('should create', () => {
