e2887bd3c8f087b4fab41c3f541bb1388bed7149
Stas Mykhailenko <stas.m@2muchcoffee.com>
2022-12-14 17:22:01 +0200

    feat (task:task-details): unsubscribe

--- tech-test/src/app/task/task-details/task-details.component.ts
+++ tech-test/src/app/task/task-details/task-details.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 import { TaskService } from '../../core/services/task.service';
-import { FormControl, FormGroup } from '@angular/forms';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
   styleUrls: ['./task-details.component.scss']
 })
-export class TaskDetailsComponent implements OnInit {
+export class TaskDetailsComponent implements OnInit, OnDestroy {
 
   currentTask$ = this.taskService.currentTask$;
 
   taskForm: FormGroup;
+  currentTaskSubscription: Subscription;
 
   get labelControl() {
     return this.taskForm.get('label') as FormControl;
@@ -44,11 +47,15 @@ export class TaskDetailsComponent implements OnInit {
       done: new FormControl(false),
     });
 
-    this.currentTask$.pipe(
+    this.currentTaskSubscription = this.currentTask$.pipe(
       filter(res => !!res),
     ).subscribe((task) => {
       this.taskForm.patchValue(task);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.currentTaskSubscription.unsubscribe();
   }
 
   editTask() {
