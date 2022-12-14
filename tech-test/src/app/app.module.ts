09acda02ac3699e8e5108c936fbb8a75eff1b83f
Stas Mykhailenko <stas.m@2muchcoffee.com>
2022-12-14 13:20:20 +0200

    feat (app): create core and shared module

--- tech-test/src/app/app.module.ts
+++ tech-test/src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    CoreModule,
     AppRoutingModule,
-    HttpClientModule
   ],
-  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ tech-test/src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+  ],
+})
+export class CoreModule {
+}
